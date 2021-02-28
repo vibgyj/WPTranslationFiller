ce45ed34c7833aa0a8e42867814a3ee9e56c8c72
Vibgy <vibgy.2008@gmail.com>
2021-02-28 14:24:24 -0500

    Adjust unit test

--- spec/googleTranslate_processPlaceholderSpaces_Spec.js
+++ spec/googleTranslate_processPlaceholderSpaces_Spec.js
@@ -3,39 +3,39 @@ describe("Google translation - processPlaceholderSpaces", function () {
     expect(
       processPlaceholderSpaces(
         "some text %shere%d",
-        "emos txet %s ereh %d"))
-      .toEqual("emos txet %sereh%d");
+        "emos txet [1] ereh [2]"))
+      .toEqual("emos txet [1]ereh[2]");
 
     expect(
       processPlaceholderSpaces(
         "some text here %1$s",
-        "emos txet ereh %1$s "))
-      .toEqual("emos txet ereh %1$s");
+        "emos txet ereh [1] "))
+      .toEqual("emos txet ereh [1]");
 
     expect(
       processPlaceholderSpaces(
         "%1$d some text here",
-        " %1$d emos txet ereh"))
-      .toEqual("%1$d emos txet ereh");
+        " [1] emos txet ereh"))
+      .toEqual("[1] emos txet ereh");
   });
 
   it("should correct space around html encodings", function () {
     expect(
       processPlaceholderSpaces(
         "some text &#8220;here&#8221;",
-        "emos txet &#8220; ereh &#8221;"))
-      .toEqual("emos txet &#8220;ereh&#8221;");
+        "emos txet [1] ereh [2]"))
+      .toEqual("emos txet [1]ereh[2]");
 
     expect(
       processPlaceholderSpaces(
         "some text here &quote;",
-        "emos txet ereh &quote; "))
-      .toEqual("emos txet ereh &quote;");
+        "emos txet ereh [1] "))
+      .toEqual("emos txet ereh [1]");
 
     expect(
       processPlaceholderSpaces(
         "&quote; some text here",
-        " &quote; emos txet ereh"))
-      .toEqual("&quote; emos txet ereh");
+        " [1] emos txet ereh"))
+      .toEqual("[1] emos txet ereh");
   });
 });
