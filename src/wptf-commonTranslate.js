356ef08a58b6f269b0b16303ea1fcda8a6950bd1
psmits1567 <peter@psmits.com>
2022-03-06 12:54:36 +0100

    06-03-2020 PSS moved translation handling into one function into commonTranslate.js 06-03-2020 PSS renamed the translator files

--- src/wptf-commonTranslate.js
+++ src/wptf-commonTranslate.js
@@ -1444,4 +1444,102 @@ function highlight(elem, keywords, caseSensitive = false, cls = "highlight") {
     }
 }
 
+// This function processes the result of the fetch
+function processTransl(original, translatedText, language, record, rowId, transtype, plural_line, locale, convertToLower, current) {
+    if (transtype == "single") {
+        textareaElem = record.querySelector("textarea.foreign-text");
+        textareaElem.innerText = translatedText;
+        // PSS 29-03-2021 Added populating the value of the property to retranslate            
+        textareaElem.value = translatedText;
+        //PSS 25-03-2021 Fixed problem with description box issue #13
+        textareaElem.style.height = "auto";
+        textareaElem.style.height = textareaElem.scrollHeight + "px";
+        current.innerText = "transFill";
+        current.value = "transFill";
+        preview = document.querySelector("#preview-" + rowId + " td.translation");
+        preview.innerText = translatedText;
+        validateEntry(language, textareaElem, "", "", rowId, locale);
+
+        // 23-09-2021 PSS if the status is not changed then sometimes the record comes back into the translation list issue #145
+        select = document.querySelector(`#editor-${rowId} div.editor-panel__right div.panel-content`);
+        //select = next_editor.getElementsByClassName("meta");
+        status = select.querySelector("dt").nextElementSibling;
+        //console.debug("bulksave status1:", select, status, rowId);
+        status.innerText = "transFill";
+    }
+    else {
+        // PSS 09-04-2021 added populating plural text
+        // PSS 09-07-2021 additional fix for issue #102 plural not updated
+        if (current != "null" && current == "current" && current == "waiting") {
+            row = rowId.split("-")[0];
+            //console.debug('rowId plural:', row)
+            textareaElem1 = f.querySelector("textarea#translation_" + row + "_0");
+        }
+        else {
+            //check_span_missing(rowId, plural_line);
+            let newrow = rowId.split("-")[1];
+            if (typeof newrow == "undefined") {
+                if (transtype != "single") {
+                    previewElem = document.querySelector("#preview-" + rowId + " li:nth-of-type(1) .translation-text");
+                    //console.debug('not single:',rowId,plural_line)
+                    if (previewElem == null) {
+                        check_span_missing(rowId, plural_line);
+                    }
+                }
+                if (plural_line == 1) {
+                    //populate plural line if not already translated, so we can take original rowId
+                    textareaElem1 = record.querySelector("textarea#translation_" + rowId + "_0");
+                    textareaElem1.innerText = translatedText;
+                    textareaElem1.value = translatedText;
+                    //PSS 25-03-2021 Fixed problem with description box issue #13
+                    textareaElem1.style.height = 'auto';
+                    textareaElem1.style.height = textareaElem1.scrollHeight + 'px';
+                    // Select the first li
+                    previewElem = document.querySelector("#preview-" + rowId + " li:nth-of-type(1) .translation-text");
+                    if (previewElem != null) {
+                        previewElem.innerText = translatedText;
+                    }
+                }
+                if (plural_line == 2) {
+                    textareaElem1 = record.querySelector("textarea#translation_" + rowId + "_1");
+                    textareaElem1.innerText = translatedText;
+                    textareaElem1.value = translatedText;
+                    // Select the second li
+                    previewElem = document.querySelector("#preview-" + rowId + " li:nth-of-type(2) .translation-text");
+                    if (previewElem != null) {
+                        previewElem.innerText = translatedText;
+                    }
+                }
+            }
+            else {
+                row = rowId.split("-")[0];
+                if (plural_line == 1) {
+                    //populate singular line if already translated
+                    textareaElem1 = record.querySelector("textarea#translation_" + row + "_0");
+                    textareaElem1.innerText = translatedText;
+                    textareaElem1.value = translatedText;
+                    previewElem = document.querySelector("#preview-" + rowId + " li:nth-of-type(1) .translation-text");
+                    if (previewElem != null) {
+                        previewElem.innerText = translatedText;
+                    }
+                }
+                else {
+                    //populate plural line if  already translated
+                    textareaElem1 = record.querySelector("textarea#translation_" + row + "_1");
+                    // console.debug('newrow = not undefined!', row + "_1");
+                    textareaElem1.innerText = translatedText;
+                    textareaElem1.value = translatedText;
+                    previewElem = document.querySelector("#preview-" + rowId + " li:nth-of-type(2) .translation-text");
+                    if (previewElem != null) {
+                        previewElem.innerText = translatedText;
+                    }
+                }
+            }
+        }
+        // The line below is necessary to update the save button on the left in the panel
+        current.innerText = "transFill";
+        current.value = "transFill";
+        validateEntry(language, textareaElem1, "", "", rowId, locale);
+    }
+}
 
