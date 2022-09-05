23062985a504fd0113ed5456500dc2957a137fa3
Peter Smits <peter@psmits.com>
2022-09-05 22:05:25 +0200

    05-09-2022 PSS fixedan issue where the start of the last sentence in a block was converted to lowercase

--- src/wptf-commonTranslate.js
+++ src/wptf-commonTranslate.js
@@ -210,7 +210,7 @@ function convert_lower(text) {
 function applySentenceCase(str) {
     // Convert each first word in a sentence to uppercase
     // 03-01-2022 PSS modified the regex issue #169
-    return str.replace(/.+?[\.\?\!\/](\s|$)/g, function (txt) {
+    return str.replace(/.+?[\.\?\!\/\. ](\s|$)/g, function (txt) {
         return txt.charAt(0).toUpperCase() + txt.substr(1);
     });
 }
